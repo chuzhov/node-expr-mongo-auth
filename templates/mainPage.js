42701412ec5c24f65341a371f91bdc23744a3125
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-02-21 18:10:13 +0200

    Updates mainpage

--- templates/mainPage.js
+++ templates/mainPage.js
@@ -1,17 +1,18 @@
 const mainPage = () => {
-  return `<!DOCTYPE html>
+  return `
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Node Server</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple Node Server</title>
 </head>
 
-<body>
-    <h1 style="background-color: black; color:aqua; 
-    display: flex; align-items: center; justify-content: center">Hello World!</h1>
+<body style="height: 100vh; background-color: black; color:aqua; 
+display: flex; align-items: center; justify-content: center"">
+  <h1 >Hello World!</h1>
 </body>
 
 </html>`;
